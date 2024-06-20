cad96fe36ed8f35e37522dcf31a43a856762f680
Stanislaw <90901059+SHEST4@users.noreply.github.com>
2024-06-20 08:51:42 +0300

    Update nmea-parser.h

--- NMEA-parser/inc/nmea-parser.h
+++ NMEA-parser/inc/nmea-parser.h
@@ -4,12 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <memory.h>
 #include <string.h>
 #include <math.h>
 
 typedef struct NMEA_RMC
 {
+	float Time;
 	uint8_t		Y[3];
 	uint8_t		M[3];
 	uint8_t		D[3];
@@ -24,6 +24,7 @@ typedef struct NMEA_RMC
 	uint8_t		EW;
 	float		Speed;
 	int8_t		Deg[128];
+	float		Course;
 }RMC;
 
 typedef struct NMEA_GGA
@@ -36,6 +37,7 @@ typedef struct NMEA_GGA
 	uint8_t		NS;
 	float		Longitude;
 	uint8_t		EW;
+	uint8_t 	Pos_Status;
 	uint8_t		Satellites;
 	float		HDOP;
 	float		Height;
@@ -51,13 +53,9 @@ typedef struct NMEA_Struct
 }NMEA;
 
 
-
 void NMEA_Parse(uint8_t* Rx_Buff, NMEA* NMEA_Struct);
 uint8_t NMEA_Parse_RMC(const uint8_t* NMEA_Data, NMEA* NMEA_Struct);
 uint8_t NMEA_Parse_GGA(const uint8_t* NMEA_Data, NMEA* NMEA_Struct);
 uint16_t NMEA_Check_Sum_Calc(const char* NMEA_Data);
 uint8_t Compare_Checksum(const char* Str);
 void Convert_Coords_To_Degs(int8_t* Degs, float* Latitude, float* Longitude, uint8_t NS, uint8_t EW);
-void print_RMC(NMEA* NMEA_Struct);
-void print_GGA(NMEA* NMEA_Struct);
-
